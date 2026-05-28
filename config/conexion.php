43d6e575609d5fee2bb09ac3be266463986f2444
Josmer Luis Alcalá García <josmerluisalcala16@gmail.com>
2026-05-28 18:12:04 -0400

    Modify database connection settings in conexion.php
    Updated MySQL user variable and corrected a typo in options.

--- config/conexion.php
+++ config/conexion.php
@@ -1,24 +1,24 @@
 <?php
 // config/conexion.php
 
-$host = $_ENV['MYSQLHOST'] ?? 'bxcfmutsnd6r952jjuzb-mysql.services.clever-cloud.com';
-$db   = $_ENV['MYSQLDATABASE'] ?? 'bxcfmutsnd6r952jjuzb';
-$user = $_ENV['MYSQLUSER'] ?? 'uaax0kegurbcatz1';
-$pass = $_ENV['MYSQLPASSWORD'] ?? 'Qk2v0FWf7POXwlOkczHP';
-$port = $_ENV['MYSQLPORT'] ?? '3306';
+$host = 'bxcfmutsnd6r952jjuzb-mysql.services.clever-cloud.com';
+$db   = 'bxcfmutsnd6r952jjuzb';
+$user = 'uaax0kegurbcatzl';
+$pass = 'Qk2v0FWf7POXwlOkczHP'; // Asegúrate que sea la O mayúscula aquí
+$port = '3306';
 $charset = 'utf8mb4';
 
 $dsn = "mysql:host=$host;dbname=$db;port=$port;charset=$charset";
 $options = [
     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES   => false,
+    PDO::ATTR_EMPLATE_PREPARES   => false,
 ];
 
 try {
-     $pdo = new PDO($dsn, $user, $pass, $options);
-     // Conexión exitosa
+    $pdo = new PDO($dsn, $user, $pass, $options);
+    // Conexión exitosa
 } catch (\PDOException $e) {
-     throw new \PDOException($e->getMessage(), (int)$e->getCode());
+    throw new \PDOException($e->getMessage(), (int)$e->getCode());
 }
 ?>
